57197c5ba8d88ccfc0805cc7b697ce4e788901c0
Benciel Cabutin <cabutinb@gmail.com>
2026-07-09 20:57:36 +0800

    Refactor server configuration and update package scripts for production builds

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,10 @@ import couponRoutes from "./routes/coupon.routes.js";
 import paymentRoutes from "./routes/payment.routes.js";
 import analyticsRoutes from "./routes/analytics.routes.js";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
-import { CLIENT_URL, PORT } from "./config/envVars.js";
+import { CLIENT_URL, NODE_ENV, PORT } from "./config/envVars.js";
 import { notFoundMiddleware } from "./middlewares/notFound.middleware.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 
@@ -25,12 +26,22 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
+const __dirname = path.resolve();
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/coupon", couponRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/analytics", analyticsRoutes);
+
+if (NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
